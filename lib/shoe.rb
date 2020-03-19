e1087bad43b9e4be183302f7df295cb7459ab9f7
Amanda Contreras <amandaben21@aol.com>
2020-03-19 23:45:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,21 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brands
+  BRANDS = ["Uggs", "Rainbow"]
 
-  def initialize(brand)
-    @brand = brand
+  def initialize(brands)
+    @brands = brands
+    BRANDS << brands
+     BRANDS.uniq!
   end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+    def brand=(brand)
+    @brand = brand
+  end
 end
+    
+   
